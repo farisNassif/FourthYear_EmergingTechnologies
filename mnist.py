cf34ccb82111c29cef42a5ebb3e43c32ce48b16a
farisNassif <g00347032@gmit.ie>
2019-10-23 19:59:34 +0000

    commented a lot of the mnist code, removed redundant testing code

--- mnist.py
+++ mnist.py
@@ -8,44 +8,29 @@ from tensorflow import keras
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
+# Used for predicting local image
 import matplotlib.image as mpimg
-# Import the fashion dataset
+
+# Import the dataset
 numbers_mnist = tf.keras.datasets.mnist
 
-# Train the images in the dataset
+# Loading the data that was imported above 
+# train_images and train_labels are the training set (What the model uses to learn)
+# test_images and test_labels are what the model is tested against after it's learned
 (train_images, train_labels), (test_images, test_labels) = numbers_mnist.load_data()
 
+# Each image is mapped to a single label. Class names are NOT included with the dataset
+# Must store them in this list to use later when plotting the images
 class_names = ['zero','one', 'two', 'three', 'four', 'five',
                'six', 'seven', 'eight', 'nine']
 
-# Plotting the first image and showing it
-'''
-plt.figure()
-plt.imshow(train_images[0])
-plt.grid(False)
-plt.show()
-'''
-
 # The images in the training set fall in the 0-255 value range
 # Will have to scale the values to a range of 0 to 1 before feeding them to the neural network model
-# To accomplish this will divide the values by 255 ~Training set and testing set must be preprocessed in the same way
+# To accomplish this will divide the values by 255 => Training set and testing set must be preprocessed in the same way
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-# Verifying the data is in the correct format, displaying the first 25 images from the *training* set
-# And also display their class name below for validation
-'''
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    # Getting rid of the annoying ticks coming out of each
-    plt.xticks([])
-    plt.yticks([])
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
-'''
-
+# Model Creation, model variable will be used below when compiling
 model = keras.Sequential([
     # First Layer - Transforms the image formats from a 2D array (of 28 x 28px) to a 1D array (of 28 x 28px)
     # This layer just reformats the data, unstacks rows of pixels and lines them up
@@ -63,7 +48,7 @@ model.compile(optimizer='adam',
 # Loss function -> Measures how accurate the model is during training
               loss='sparse_categorical_crossentropy',
 # Metrics -> Used to monitor the training and testing steps
-              metrics=['accuracy'])
+              metrics=['accuracy'],)
 
 # Train the model
 
