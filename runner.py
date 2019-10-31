b5abeec631a50c2cd9e2894c517375ad6b638442
farisNassif <g00347032@gmit.ie>
2019-10-31 16:59:11 +0000

    ajax call to flask correctly working, need to send and save canvas as .png

--- runner.py
+++ runner.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import os
+import matplotlib.pyplot as plt
 # Local file for running the model
 import runmnist as rm
+import uuid
 # Having some issues with tensorflow and its depriciated packages
 
 app = Flask(__name__)
@@ -17,9 +19,16 @@ def index():
                        title=title)
 
 # Upload/Publish route
-@app.route('/predict', methods=['Post'])
+@app.route('/upload', methods=['Post'])
 def upload():
-    return "posted to backend!"
+    image_b64=request.values[('imageBase64')]
+    return "jeje"
+
+def create_csv(text):
+    unique_id = str(uuid.uuid4())
+    with open('images/'+unique_id+'.png', 'a') as file:
+        file.write(text[1:-1]+"\n")
+    return unique_id
 
 if __name__ == "__main__":
     # If theres any errors they'll pop up on the page
