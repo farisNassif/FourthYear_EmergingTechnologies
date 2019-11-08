a3e414de63d5359c2c14fa7cd15ec52207ac1c7a
farisNassif <g00347032@gmit.ie>
2019-11-08 13:22:45 +0000

    value inversion function added for inverting the canvas image

--- app/utilities.py
+++ app/utilities.py
@@ -7,8 +7,10 @@ from PIL import ImageOps as io
 
 """
 This Class contains utility functions that can TODO
+
 """
 
+# Takes in an image and 
 def image_to_array(image):
     # Resize the image
     image = io.fit(image, (28,28))
@@ -27,6 +29,22 @@ def image_to_array(image):
     alpha_composite = Image.alpha_composite(background, sized)
     alpha_composite.save('toArray', 'PNG', quality=80)
 
-    img = cv2.imread("toArray.png", cv2.IMREAD_GRAYSCALE)
+    # Loads the image as greyscale
+    image_array = cv2.imread("toArray.png", cv2.IMREAD_GRAYSCALE)
 
-    return img  
+    return image_array  
+
+# Takes an array as a paramater which should only contain values between 0 and 1
+# The idea is to invert the canvas image as it would be inverted if it were to be an MNIST image
+# https://stackoverflow.com/questions/19580102/inverting-image-in-python-with-opencv
+def invert_values(image_array):
+
+    # Need to make the array flat to allow looping
+    array_flattened = image_array.flatten()
+
+    # Inverts the values within the array { x => 1 - x } |E.G| { 0.4 = 1 - 0.4 } => x is now 0.6
+    for i in range(array_flattened.size):
+        array_flattened[i] = 1 - image_array[i]
+
+    # Return the altered array with the same shape it had before
+    return array_flattened.reshape(image_array.shape)
